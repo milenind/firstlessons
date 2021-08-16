ceaf957c9ebb30f7562cd9f6972cc55c68e400e8
milenind <dima.milenin09@gmail.com>
2021-08-16 17:37:01 +0300

    upd

--- eighth/DB.php
+++ eighth/DB.php
@@ -1,4 +1,29 @@
 <?php
-class DB{
 
+class DB
+{
+    protected $db;
+
+    public function __construct()
+    {
+        $this->db = new PDO('mysql:host=localhost;dbname=eighth', 'root', 'root');
+    }
+
+    public function execute(string $sql): bool
+    {
+        $sth = $this->db->prepare($sql);
+        return $sth->execute();
+    }
+
+    /**
+     * @param string $sql
+     * @param array $data
+     * @return array|false
+     */
+    public function query(string $sql, array $data)
+    {
+        $sth = $this->db->prepare($sql);
+        $sth->execute($data);
+        return $sth->fetchall();
+    }
 }
---
+++ eighth/eighth.php
@@ -0,0 +1,15 @@
+<?php
+require 'DB.php';
+
+$db = new DB();
+
+$queryString = 'SELECT * FROM articles';
+$res = $db->execute($queryString);
+$sql = 'select * from articles where id = :id or name = :name or author=:author or text=:text';
+$data = [':id' => $_GET['id'], ':name' => $_GET['name'],':author'=>$_GET['author'],':text' => $_GET['text']];
+$resQuery = $db->query($sql, $data);
+echo '<pre>';
+foreach ($resQuery as $item) {
+    echo $item['id'] . ' ' . $item['name'] . ' ' . $item['author']. ' ' . $item['text'];
+}
+
