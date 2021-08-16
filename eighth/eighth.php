81f9ea153b91b05f3b665475493633c7178378ad
milenind <dima.milenin09@gmail.com>
2021-08-16 17:59:20 +0300

    upd

--- eighth/eighth.php
+++ eighth/eighth.php
@@ -6,10 +6,10 @@ $db = new DB();
 $queryString = 'SELECT * FROM articles';
 $res = $db->execute($queryString);
 $sql = 'select * from articles where id = :id or name = :name or author=:author or text=:text';
-$data = [':id' => $_GET['id'], ':name' => $_GET['name'],':author'=>$_GET['author'],':text' => $_GET['text']];
+$data = [':id' => $_GET['id'], ':name' => $_GET['name'], ':author' => $_GET['author'], ':text' => $_GET['text']];
 $resQuery = $db->query($sql, $data);
 echo '<pre>';
 foreach ($resQuery as $item) {
-    echo $item['id'] . ' ' . $item['name'] . ' ' . $item['author']. ' ' . $item['text'];
+    echo $item['id'] .' '. $item['name'] .' '. $item['author'] . ' ' . $item['text'];
 }
 
